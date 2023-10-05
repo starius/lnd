1ce111d72a21977683c1deddced83e9ac10bfed7
Boris Nagaev <bnagaev@gmail.com>
2023-10-05 00:10:22 -0300

    lnrpc: API to backup db channels

--- lnrpc/lightning.pb.gw.go
+++ lnrpc/lightning.pb.gw.go
@@ -2098,9 +2098,20 @@ func local_request_Lightning_ExportChannelBackup_0(ctx context.Context, marshale
 
 }
 
+var (
+	filter_Lightning_ExportAllChannelBackups_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_Lightning_ExportAllChannelBackups_0(ctx context.Context, marshaler runtime.Marshaler, client LightningClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ChanBackupExportRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Lightning_ExportAllChannelBackups_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.ExportAllChannelBackups(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -2110,6 +2121,13 @@ func request_Lightning_ExportAllChannelBackups_0(ctx context.Context, marshaler 
 func local_request_Lightning_ExportAllChannelBackups_0(ctx context.Context, marshaler runtime.Marshaler, server LightningServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ChanBackupExportRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Lightning_ExportAllChannelBackups_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.ExportAllChannelBackups(ctx, &protoReq)
 	return msg, metadata, err
