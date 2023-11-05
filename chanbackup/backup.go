7b2eb0b5ff05d3b1c86b76e04fb97d85e2ad91c2
Boris Nagaev <bnagaev@gmail.com>
2023-11-05 19:43:33 -0300

    rpcserver, chanbackup: save CloseTx in chanbackup

--- chanbackup/backup.go
+++ chanbackup/backup.go
@@ -1,13 +1,16 @@
 package chanbackup
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/kvdb"
+	"github.com/lightningnetwork/lnd/lnwallet"
 )
 
 // LiveChannelSource is an interface that allows us to query for the set of
@@ -53,11 +56,44 @@ func assembleChanBackup(addrSource AddressSource,
 	return &single, nil
 }
 
+func buildCloseTx(targetChan *channeldb.OpenChannel, signer input.Signer) ([]byte, error) {
+	chanMachine, err := lnwallet.NewLightningChannel(signer, targetChan, nil)
+	if err != nil {
+		return nil, fmt.Errorf("lnwallet.NewLightningChannel failed: %w", err)
+	}
+	closeSummary, err := chanMachine.GetLocalForceCloseSummary()
+	if err != nil {
+		return nil, fmt.Errorf("GetLocalForceCloseSummary failed: %w", err)
+	}
+	var txBuf bytes.Buffer
+	if err := closeSummary.CloseTx.Serialize(&txBuf); err != nil {
+		return nil, fmt.Errorf("CloseTx.Serialize failed: %w", err)
+	}
+	return txBuf.Bytes(), nil
+}
+
+type BackupConfig struct {
+	signer input.Signer
+}
+
+type BackupOption func(*BackupConfig)
+
+func WithCloseTx(signer input.Signer) BackupOption {
+	return func(c *BackupConfig) {
+		c.signer = signer
+	}
+}
+
 // FetchBackupForChan attempts to create a plaintext static channel backup for
 // the target channel identified by its channel point. If we're unable to find
 // the target channel, then an error will be returned.
 func FetchBackupForChan(chanPoint wire.OutPoint, chanSource LiveChannelSource,
-	addrSource AddressSource) (*Single, error) {
+	addrSource AddressSource, options ...BackupOption) (*Single, error) {
+
+	var config BackupConfig
+	for _, opt := range options {
+		opt(&config)
+	}
 
 	// First, we'll query the channel source to see if the channel is known
 	// and open within the database.
@@ -75,13 +111,27 @@ func FetchBackupForChan(chanPoint wire.OutPoint, chanSource LiveChannelSource,
 		return nil, fmt.Errorf("unable to create chan backup: %v", err)
 	}
 
+	if config.signer != nil {
+		// Add CloseTx.
+		closeTx, err := buildCloseTx(targetChan, config.signer)
+		if err != nil {
+			return nil, fmt.Errorf("unable to create close tx for ChannelPoint(%v): %w", targetChan, err)
+		}
+		staticChanBackup.CloseTx = closeTx
+	}
+
 	return staticChanBackup, nil
 }
 
 // FetchStaticChanBackups will return a plaintext static channel back up for
 // all known active/open channels within the passed channel source.
 func FetchStaticChanBackups(chanSource LiveChannelSource,
-	addrSource AddressSource) ([]Single, error) {
+	addrSource AddressSource, options ...BackupOption) ([]Single, error) {
+
+	var config BackupConfig
+	for _, opt := range options {
+		opt(&config)
+	}
 
 	// First, we'll query the backup source for information concerning all
 	// currently open and available channels.
@@ -98,6 +148,15 @@ func FetchStaticChanBackups(chanSource LiveChannelSource,
 		chanBackup, err := assembleChanBackup(addrSource, openChan)
 		if err != nil {
 			return nil, err
+		}
+
+		if config.signer != nil {
+			// Add CloseTx.
+			closeTx, err := buildCloseTx(openChan, config.signer)
+			if err != nil {
+				return nil, fmt.Errorf("unable to create close tx for ChannelPoint(%v): %w", openChan, err)
+			}
+			chanBackup.CloseTx = closeTx
 		}
 
 		staticChanBackups = append(staticChanBackups, *chanBackup)
