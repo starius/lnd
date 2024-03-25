6477bec6ba8160bea3992be1f19fd784d7145b77
Boris Nagaev <bnagaev@gmail.com>
2024-03-25 00:30:29 -0300

    rpcserver: include unsigned close tx in backups
    RPCs ExportChannelBackup, ExportAllChannelBackups and SubscribeChannelBackups
    now return backups with the material needed to produce a force-close tx,
    given the private key is available at restore time.

--- rpcserver.go
+++ rpcserver.go
@@ -7485,6 +7485,7 @@ func (r *rpcServer) ExportChannelBackup(ctx context.Context,
 	// unknown, then we'll return an error
 	unpackedBackup, err := chanbackup.FetchBackupForChan(
 		chanPoint, r.server.chanStateDB, r.server.addrSource,
+		chanbackup.WithCloseTxInputs(),
 	)
 	if err != nil {
 		return nil, err
@@ -7655,6 +7656,7 @@ func (r *rpcServer) ExportAllChannelBackups(ctx context.Context,
 	// channels from disk.
 	allUnpackedBackups, err := chanbackup.FetchStaticChanBackups(
 		r.server.chanStateDB, r.server.addrSource,
+		chanbackup.WithCloseTxInputs(),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("unable to fetch all static chan "+
@@ -7784,6 +7786,7 @@ func (r *rpcServer) SubscribeChannelBackups(req *lnrpc.ChannelBackupSubscription
 			// backups from disk.
 			chanBackups, err := chanbackup.FetchStaticChanBackups(
 				r.server.chanStateDB, r.server.addrSource,
+				chanbackup.WithCloseTxInputs(),
 			)
 			if err != nil {
 				return fmt.Errorf("unable to fetch all "+
