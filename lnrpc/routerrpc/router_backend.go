0c231f9d1545582ca8f39ce58024f1fb86264b07
Calvin Zachman <calvin.zachman@protonmail.com>
2024-08-30 19:27:50 -0400

    lnrpc: allow arbitrary route source
    The RouterBackend assumes that the source public
    key for a route is always the node’s public key
    when calling UnmarshallRoute.
    
    Here we update to allow overriding of this field
    to any arbitrary public key.

--- lnrpc/routerrpc/router_backend.go
+++ lnrpc/routerrpc/router_backend.go
@@ -591,6 +591,7 @@ func (r *RouterBackend) MarshallRoute(route *route.Route) (*lnrpc.Route, error) 
 		TotalAmtMsat:       int64(route.TotalAmount),
 		Hops:               make([]*lnrpc.Hop, len(route.Hops)),
 		FirstHopAmountMsat: int64(route.FirstHopAmount.Val.Int()),
+		SourcePubKey:       route.SourcePubKey.String(),
 	}
 
 	// Encode the route's custom channel data (if available).
@@ -773,6 +774,20 @@ func (r *RouterBackend) UnmarshallHop(rpcHop *lnrpc.Hop,
 func (r *RouterBackend) UnmarshallRoute(rpcroute *lnrpc.Route) (
 	*route.Route, error) {
 
+	var sourcePubKey route.Vertex
+	var err error
+
+	// If SourcePubKey is provided, use it, otherwise default to SelfNode.
+	if rpcroute.SourcePubKey != "" {
+		sourcePubKey, err = route.NewVertexFromStr(rpcroute.SourcePubKey)
+		if err != nil {
+			return nil, fmt.Errorf("invalid source pubkey: %v", err)
+		}
+	} else {
+		sourcePubKey = r.SelfNode
+	}
+
+	// prevNodePubKey := sourcePubKey
 	prevNodePubKey := r.SelfNode
 
 	hops := make([]*route.Hop, len(rpcroute.Hops))
@@ -783,14 +798,13 @@ func (r *RouterBackend) UnmarshallRoute(rpcroute *lnrpc.Route) (
 		}
 
 		hops[i] = routeHop
-
 		prevNodePubKey = routeHop.PubKeyBytes
 	}
 
 	route, err := route.NewRouteFromHops(
 		lnwire.MilliSatoshi(rpcroute.TotalAmtMsat),
 		rpcroute.TotalTimeLock,
-		r.SelfNode,
+		sourcePubKey,
 		hops,
 	)
 	if err != nil {
